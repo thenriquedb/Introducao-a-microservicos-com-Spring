528a57537c9ef197355a7bd45febf644c74d506f
thenriquedb <thenrique2012@gmail.com>
2025-05-14 15:08:26 -0300

    feat(ms-users): add User entity with fields for id, name, and email

---
+++ ms-users/src/main/java/com/thenriquedb/ms_users/domain/User.java
@@ -0,0 +1,22 @@
+package com.thenriquedb.ms_users.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private String email;
+}
