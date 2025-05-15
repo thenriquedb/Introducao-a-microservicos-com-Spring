f690d0d9f14689fd62c4a9e3c15adb5bf3869667
thenriquedb <thenrique2012@gmail.com>
2025-05-15 12:29:24 -0300

    feat(ms-email): enhance EmailConsumer to process and send emails from the queue

--- ms-email/src/main/java/com/thenriquedb/ms_email/consumers/EmailConsumer.java
+++ ms-email/src/main/java/com/thenriquedb/ms_email/consumers/EmailConsumer.java
@@ -1,14 +1,26 @@
 package com.thenriquedb.ms_email.consumers;
 
+import com.thenriquedb.ms_email.domain.Email;
 import com.thenriquedb.ms_email.dtos.EmailRecordDto;
+import com.thenriquedb.ms_email.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
+    @Autowired
+    EmailService emailService;
+
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto) {
-        System.out.println("Email content: " + emailRecordDto.userId().toString());
+        Email email = new Email();
+        BeanUtils.copyProperties(emailRecordDto, email);
+
+        System.out.println("Received email: " + email);
+
+        emailService.sendEmail(email);
     }
 }
