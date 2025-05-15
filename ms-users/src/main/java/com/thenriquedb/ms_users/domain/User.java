d91b05e3f16f60c059f7be70d15a49ac60e762a5
thenriquedb <thenrique2012@gmail.com>
2025-05-15 12:29:32 -0300

    feat(user): add unique constraint to email field in User entity

--- ms-users/src/main/java/com/thenriquedb/ms_users/domain/User.java
+++ ms-users/src/main/java/com/thenriquedb/ms_users/domain/User.java
@@ -17,6 +17,9 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
+
+    @Column(unique = true)
     private String email;
 }
